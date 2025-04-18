92c1363e521396e60d3626f5b99f037406112491
Zain Ali <zainidrees3830@gmail.com>
2025-04-18 00:27:34 -0700

    color changes

--- src/app/(DashboardLayout)/components/dashboard/TopPayingClients.tsx
+++ src/app/(DashboardLayout)/components/dashboard/TopPayingClients.tsx
@@ -56,182 +56,207 @@ const products = [
     deadline: "Table item title",
     fileupload: "Table item title",
     status: "Table item title",
-  }
+  },
 ];
 
 const TopPayingClients = () => {
   return (
-    (
-      <div id="table">
-         <DashboardCard title="Assignments"  >
-        <Box sx={{display:"flex",justifyContent:'space-between'}}>
-        <Input placeholder="Filter Lines" sx={{color:'white', border:'', borderRadius:'5px', paddingLeft:'5px'}}></Input>
-<Select
-  defaultValue=""
-  displayEmpty
-  variant="standard"
-  sx={{
-    height: '30px',
-    width: '120px',
-    borderRadius: '5px',
-    padding: '5px',
-    color: 'white',
-    backgroundColor: '#ba25ee',
-  }}
-  MenuProps={{
-    PaperProps: {
-      sx: {
-        backgroundColor: '#ba25ee', // 💜 dropdown ka full background
-        color: 'white',              // text color
-        '& .MuiMenuItem-root': {
-          color: 'white',           // individual item text
-          '&:hover': {
-            backgroundColor: '#a020f0', // hover color
-          },
-        },
-      },
-    },
-  }}
->
-  <MenuItem value="" sx={{ display: 'none' }}>
-    Columns
-  </MenuItem>
-  <MenuItem value="option1">Option 1</MenuItem>
-  <MenuItem value="option2">Option 2</MenuItem>
-  <MenuItem value="option3">Option 3</MenuItem>
-</Select>
-
-        </Box>
-      <Box sx={{ overflow: "auto" }}>
-        <Box mt={2} sx={{ width: "100%", display: "table", tableLayout: "fixed" }}>
-          <Table
+    <div id="table">
+      <DashboardCard title="Assignments">
+        <Box sx={{ display: "flex", justifyContent: "space-between" }}>
+          <Input
+            placeholder="Filter Lines"
             sx={{
-              whiteSpace: "nowrap",
+              color: "white",
+              border: "",
+              borderRadius: "5px",
+              paddingLeft: "5px",
+            }}
+          ></Input>
+          <Select
+            defaultValue=""
+            displayEmpty
+            variant="standard"
+            sx={{
+              height: "30px",
+              width: "120px",
+              borderRadius: "5px",
+              padding: "5px",
+              color: "white",
+              backgroundColor: "#ba25ee",
+            }}
+            MenuProps={{
+              PaperProps: {
+                sx: {
+                  backgroundColor: "#ba25ee", // 💜 dropdown ka full background
+                  color: "white", // text color
+                  "& .MuiMenuItem-root": {
+                    color: "white", // individual item text
+                    "&:hover": {
+                      backgroundColor: "#a020f0", // hover color
+                    },
+                  },
+                },
+              },
             }}
           >
-            <TableHead >
-              <TableRow >
-                <TableCell>
-                  <Typography variant="subtitle2" sx={{
-                    fontWeight: 600,
-                    color:'white'
-                  }}>
-                    Tittle 
-                  </Typography>
-                </TableCell>
-                <TableCell>
-                  <Typography variant="subtitle2" sx={{
-                    fontWeight: 600,
-                    color:'white'
-                  }}>
-                    Instruction
-                  </Typography>
-                </TableCell>
-                <TableCell>
-                  <Typography variant="subtitle2" sx={{
-                    fontWeight: 600,
-                    color:'white'
-                  }}>
-                    Deadline
-                  </Typography>
-                </TableCell>
-                <TableCell>
-                  <Typography variant="subtitle2" sx={{
-                    fontWeight: 600,
-                    color:'white'
-                  }}>
-                    File Upload/URL
-                  </Typography>
-                </TableCell>
-                <TableCell align="right">
-                  <Typography variant="subtitle2" sx={{
-                    fontWeight: 600,
-                    color:'white',
-                    marginRight:'50px'
-                  }}>
-                    Status
-                  </Typography>
-                </TableCell>
-              </TableRow>
-            </TableHead>
-            <TableBody>
-              {products.map((product) => (
-                <TableRow key={product.name}>
+            <MenuItem value="" sx={{ display: "none" }}>
+              Columns
+            </MenuItem>
+            <MenuItem value="option1">Option 1</MenuItem>
+            <MenuItem value="option2">Option 2</MenuItem>
+            <MenuItem value="option3">Option 3</MenuItem>
+          </Select>
+        </Box>
+        <Box sx={{ overflow: "auto" }}>
+          <Box
+            mt={2}
+            sx={{ width: "100%", display: "table", tableLayout: "fixed" }}
+          >
+            <Table
+              sx={{
+                whiteSpace: "nowrap",
+              }}
+            >
+              <TableHead>
+                <TableRow>
                   <TableCell>
                     <Typography
-                      sx={{
-                        fontSize: "15px",
-                        fontWeight: "500",
-                        color:'white'
-                      }}
-                    >
-                      {product.id}
-                    </Typography>
-                  </TableCell>
-                  <TableCell>
-                    <Box
-                      sx={{
-                        display: "flex",
-                        alignItems: "center",
-                        color:'white'
-                      }}
-                    >
-                      <Box>
-                        <Typography variant="subtitle2" sx={{
-                          fontWeight: 600,
-                          color:'white'
-                        }}>
-                          {product.title}
-                        </Typography>
-                        <Typography
-                          color="textSecondary"
-                          sx={{
-                            fontSize: "13px",
-                            color:'white'
-                          }}
-                        >
-                          {product.instruction}
-                        </Typography>
-                      </Box>
-                    </Box>
-                  </TableCell>
-                  <TableCell>
-                    <Typography
-                      color="textSecondary"
                       variant="subtitle2"
                       sx={{
-                        fontWeight: 400,
-                        color:'white'
+                        fontWeight: 600,
+                        color: "white",
                       }}
                     >
-                      {product.deadline}
+                      Tittle
                     </Typography>
                   </TableCell>
                   <TableCell>
-                    <Chip
+                    <Typography
+                      variant="subtitle2"
                       sx={{
-                        px: "4px",
-                        backgroundColor:"#0d113b",
-                        // color: "#fff",
-                        color:'white'
+                        fontWeight: 600,
+                        color: "white",
                       }}
-                      size="small"
-                      label={product.fileupload}
-                    ></Chip>
+                    >
+                      Instruction
+                    </Typography>
                   </TableCell>
-                  <TableCell align="right"sx={{color:'white'}}>
-                    <Typography variant="h6">${product.status}k</Typography>
+                  <TableCell>
+                    <Typography
+                      variant="subtitle2"
+                      sx={{
+                        fontWeight: 600,
+                        color: "white",
+                      }}
+                    >
+                      Deadline
+                    </Typography>
+                  </TableCell>
+                  <TableCell>
+                    <Typography
+                      variant="subtitle2"
+                      sx={{
+                        fontWeight: 600,
+                        color: "white",
+                      }}
+                    >
+                      File Upload/URL
+                    </Typography>
+                  </TableCell>
+                  <TableCell align="right">
+                    <Typography
+                      variant="subtitle2"
+                      sx={{
+                        fontWeight: 600,
+                        color: "white",
+                        marginRight: "50px",
+                      }}
+                    >
+                      Status
+                    </Typography>
                   </TableCell>
                 </TableRow>
-              ))}
-            </TableBody>
-          </Table>
+              </TableHead>
+              <TableBody>
+                {products.map((product) => (
+                  <TableRow key={product.name}>
+                    <TableCell>
+                      <Typography
+                        sx={{
+                          fontSize: "15px",
+                          fontWeight: "500",
+                          color: "white",
+                        }}
+                      >
+                        {product.id}
+                      </Typography>
+                    </TableCell>
+                    <TableCell>
+                      <Box
+                        sx={{
+                          display: "flex",
+                          alignItems: "center",
+                          color: "white",
+                        }}
+                      >
+                        <Box>
+                          <Typography
+                            variant="subtitle2"
+                            sx={{
+                              fontWeight: 600,
+                              color: "white",
+                            }}
+                          >
+                            {product.title}
+                          </Typography>
+                          <Typography
+                            color="textSecondary"
+                            sx={{
+                              fontSize: "13px",
+                              color: "white",
+                            }}
+                          >
+                            {product.instruction}
+                          </Typography>
+                        </Box>
+                      </Box>
+                    </TableCell>
+                    <TableCell>
+                      <Typography
+                        color="textSecondary"
+                        variant="subtitle2"
+                        sx={{
+                          fontWeight: 400,
+                          color: "white",
+                        }}
+                      >
+                        {product.deadline}
+                      </Typography>
+                    </TableCell>
+                    <TableCell>
+                      <Chip
+                        sx={{
+                          px: "4px",
+                          backgroundColor: "#0d113b",
+                          // color: "#fff",
+                          color: "white",
+                        }}
+                        size="small"
+                        label={product.fileupload}
+                      ></Chip>
+                    </TableCell>
+                    <TableCell align="right" sx={{ color: "white" }}>
+                      <Typography variant="h6">${product.status}k</Typography>
+                    </TableCell>
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
+          </Box>
         </Box>
-      </Box>
-    </DashboardCard>
-
-      </div>
-         )
+      </DashboardCard>
+    </div>
   );
 };
 
